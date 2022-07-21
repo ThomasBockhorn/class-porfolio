b71edd54e5df7673bbc81b2e6fc9092baadfc972
Thomas Bockhorn <historian.thomas@gmail.com>
2022-07-20 22:55:25 -0500

    User gets notification when a profile is successfully changed

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -79,7 +79,12 @@ class AdminController extends Controller
 
         $adminUser->save();
 
-        return redirect()->route('admin.profile');
+        $notification = [
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success'
+        ];
+
+        return redirect()->route('admin.profile')->with($notification);
 
     }
 }
--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -29,6 +29,9 @@
     <link href="{{asset('backend/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
     <link href="{{asset('backend/assets/css/app-dark.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
 
+    <!-- Toastr -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+
 
 </head>
 
@@ -79,6 +82,32 @@
 
 <!-- App js -->
 <script src="{{ asset('backend/assets/js/app.js')}}"></script>
+
+<!--toastr -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 </body>
+
+<script>
+    @if(Session::has('message'))
+    let type = "{{ Session::get('alert-type','info') }}"
+    switch(type){
+        case 'info':
+            toastr.info(" {{ Session::get('message') }} ");
+            break;
+
+        case 'success':
+            toastr.success(" {{ Session::get('message') }} ");
+            break;
+
+        case 'warning':
+            toastr.warning(" {{ Session::get('message') }} ");
+            break;
+
+        case 'error':
+            toastr.error(" {{ Session::get('message') }} ");
+            break;
+    }
+    @endif
+</script>
 
 </html>
