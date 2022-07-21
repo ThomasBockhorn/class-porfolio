202e09d59233442b690a8e9fb33d2f023ee2ce32
Thomas Bockhorn <historian.thomas@gmail.com>
2022-07-21 18:29:53 -0500

    Added notification to login and logout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,6 +18,9 @@
     <!-- App Css-->
     <link href="{{ asset('/backend/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 
+    <!-- Toastr -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+
 </head>
 
 <body class="auth-body-bg">
@@ -98,5 +101,31 @@
 
 <script src="{{ asset('/backend/assets/js/app.js') }}"></script>
 
+<!--toastr -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+</body>
+
+<script>
+    @if(Session::has('message'))
+    let type = "{{ Session::get('alert-type','info') }}"
+    switch(type){
+        case 'info':
+            toastr.info(" {{ Session::get('message') }} ");
+            break;
+
+        case 'success':
+            toastr.success(" {{ Session::get('message') }} ");
+            break;
+
+        case 'warning':
+            toastr.warning(" {{ Session::get('message') }} ");
+            break;
+
+        case 'error':
+            toastr.error(" {{ Session::get('message') }} ");
+            break;
+    }
+    @endif
+</script>
 </body>
 </html>
